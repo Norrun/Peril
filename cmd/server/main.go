160c997d08f3fe920134357d43292cad30f6c9c4
Norrun <trulvens@tutanota.com>
2026-05-20 23:37:30 +0200

    publishing pause message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/nhelps"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -17,6 +19,13 @@ func main() {
 		log.Fatalln("Failed to connect(Dial) to amqp server: ", err)
 	}
 	defer nhelps.RunLogErr(con.Close, "error closing connection to amqp server")
+
+	qChan, err := con.Channel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	pubsub.PublishJSON(qChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+
 	log.Println("Connection to broker succesfull")
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
