9855b634f200c53625aea43e55fa396d6fdbd190
Norrun <trulvens@tutanota.com>
2026-05-21 17:36:10 +0200

    server REPL (Decoupling)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/nhelps"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
@@ -13,20 +15,49 @@ import (
 
 func main() {
 	log.Println("Starting Peril server...")
-	const conStr string = "amqp://guest:guest@localhost:5672/"
-	con, err := amqp.Dial(conStr)
+	gamelogic.PrintServerHelp()
+
+	con, err := amqp.Dial(pubsub.ConnectStr)
 	if err != nil {
 		log.Fatalln("Failed to connect(Dial) to amqp server: ", err)
 	}
 	defer nhelps.RunLogErr(con.Close, "error closing connection to amqp server")
-
+	log.Println("Connection to broker succesfull")
 	qChan, err := con.Channel()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	pubsub.PublishJSON(qChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+		stop := false
+		switch input[0] {
+		case routing.PauseKey:
+			fmt.Println("Pausing")
+			pubsub.PublishJSON(qChan,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{IsPaused: true})
+		case "resume":
+			fmt.Println("Resuming")
+			pubsub.PublishJSON(qChan,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{IsPaused: false})
+		case "quit":
+			fmt.Println("Quitting")
+			stop = true
+		default:
+			fmt.Println("Unknown command")
+		}
+		if stop {
+			break
+		}
 
-	log.Println("Connection to broker succesfull")
+	}
+
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
 	<-sigChan
